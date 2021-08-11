a735f676067ca3cff39c230f5995c9eecea231f8
apexakhil007@gmail.com <apexakhil007@gmail.com>
2021-08-11 18:03:39 +0530

    added employee jpa

--- spring-jdbc/src/main/java/com/fis/springjdbc/employee/EmployeeDAOImpl.java
+++ spring-jdbc/src/main/java/com/fis/springjdbc/employee/EmployeeDAOImpl.java
@@ -9,36 +9,46 @@ import com.fis.springjdbc.product.ProductMapper;
 
 public class EmployeeDAOImpl extends JdbcDaoSupport implements IEmployee{
 
-	@Override
 	public void insertEmployee(Employee employee) {
 		// TODO Auto-generated method stub
-		
+		String sql = "insert into employee values(?,?)";
+		Object[] args = { employee.getId(), employee.getName() };
+		int insertedRows = getJdbcTemplate().update(sql, args);
+		System.out.println("Rows Inserted: " + insertedRows);
 	}
 
-	@Override
 	public List<Employee> getAllEmployee() {
 		// TODO Auto-generated method stub
 		String sql = "select * from product";
-		List<Employee> products = getJdbcTemplate().query(sql, new ProductMapper());
+		List<Employee> employee = getJdbcTemplate().query(sql, new EmployeeMapper());
 		return employee;
 	}
 
-	@Override
 	public void updateEmployee(Employee employee) {
 		// TODO Auto-generated method stub
+		String sql = "update employee set name=? where id=?";
+		Object[] args = { employee.getName(), employee.getId() };
+		int updatedRows = getJdbcTemplate().update(sql, args);
+		System.out.println("Rows Updated: " + updatedRows);
 		
 	}
 
 	@Override
 	public void deleteEmployee(int id) {
 		// TODO Auto-generated method stub
-		
+		String sql = "delete from employee where id=?";
+		Object[] args = { id };
+		int deletedRows = getJdbcTemplate().update(sql, args);
+		System.out.println("Rows Deleted: " + deletedRows);
 	}
 
 	@Override
 	public Employee getEmployeeById(int id) {
 		// TODO Auto-generated method stub
-		return null;
+		String sql = "select * from product where id=?";
+		Object[] args = { id };
+		Employee employeeById = getJdbcTemplate().queryForObject(sql, args, new EmployeeMapper());
+		return employeeById;
 	}
 
 }
